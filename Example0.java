5eafa593adae0f1d8ab9c5f9ab9cc6e7ac901201
Uğur İnal <ugurinal01@hotmail.com>
2019-06-30 02:14:48 +0300

    Create Example0.java
    Example0

---
+++ Example0.java
@@ -0,0 +1,39 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    int highScorePosition = calculateHighScorePosition(1500);
+    displayHighScorePosition("UGUR0",highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(900);
+    displayHighScorePosition("UGUR1",highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(400);
+    displayHighScorePosition("UGUR2",highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(50);
+    displayHighScorePosition("UGUR3",highScorePosition);
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int position){
+
+        System.out.println(playerName + " is managed to get into position "+position +" on the high score table.");
+
+
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+
+        if(playerScore >=1000){
+            return 1;
+        }else if((playerScore >= 500) && playerScore < 1000){
+            return 2;
+        }else if((playerScore >=100) && (playerScore < 500)){
+            return 3;
+        }else{
+            return 4;
+        }
+
+    }
+}
