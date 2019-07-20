9590f9ac277c1b7b8d0988ccafecd5f190d11b43
Uğur İnal <ugurinal01@hotmail.com>
2019-07-20 21:42:45 +0300

    Update sumOfOddv2.java

--- sumOfOddv2.java
+++ sumOfOddv2.java
@@ -37,19 +37,31 @@ public class Main {
 
         int start;
         int end;
+        boolean check;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the starting value: ");
 
-        input.hasNextInt();     /// Checking the value whether it is int or not
-        start = input.nextInt();
+        check = input.hasNextInt();     /// Checking the value whether it is int or not
+        if(check){
+            start = input.nextInt();
+            System.out.println("Enter the last value: ");
+            check = input.hasNextInt();
+            if (check){
+                end = input.nextInt();
+                System.out.println("The sum of odd numbers from "+start +" to "+end +" is " +sumOdd(start,end));
 
-        System.out.println("Enter the last value: ");
+            } else{
+                System.out.println("You enterd invalid value...");
+            }
 
-        input.hasNextInt();     /// Checking the value whether it is int or not
-        end = input.nextInt();
+        }else{
+            System.out.println("You entered invalid value...");
+        }
+
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //                                                                                                      //
         // if we are going to enter String we have to deal Line ( \n enter ) issue. To solve this we must add   //
         // extra input.NextLine(); line before we take the string from user...                                  //
         // E.g.                                                                                                 //
@@ -63,8 +75,6 @@ public class Main {
         //                                                                                                      //
         //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-        System.out.println("The sum of odd numbers from "+start +" to "+end +" is " +sumOdd(start,end));
 
         input.close();
 
