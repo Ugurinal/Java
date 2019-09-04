54a6c1a33010ea41ac802a945554d79509c15ff7
Uğur İnal <ugurinal01@hotmail.com>
2019-09-04 16:32:42 +0300

    solution 3
    solution 3

--- Java. Statements and Arrays/Switch statements/Programming (3)/src/Main.java
+++ Java. Statements and Arrays/Switch statements/Programming (3)/src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String shape = input.nextLine();
+
+
+        switch (shape) {
+            case "triangle":
+                double a = input.nextDouble();
+                double b = input.nextDouble();
+                double c = input.nextDouble();
+
+                double u = (a + b + c) / 2;
+                double area = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+                System.out.println(area);
+                break;
+            case "rectangle":
+                double a1 = input.nextDouble();
+                double b1 = input.nextDouble();
+                System.out.println(a1 * b1);
+                break;
+            case "circle":
+                double r = input.nextDouble();
+                System.out.println(3.14 * Math.pow(r, 2));
+                break;
+            default:
+                System.out.println("Error!");
+        }
+    }
 }
