82f1baa3f8603a7cea834e99bbb170bef46b4af9
Uğur İnal <ugurinal01@hotmail.com>
2019-09-04 18:36:59 +0300

    #Solutions
    #Solutions

--- Java. Statements and Arrays/While and do-while loops/Programming (2)/src/Main.java
+++ Java. Statements and Arrays/While and do-while loops/Programming (2)/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int input;
+
+        do {
+            input = scanner.nextInt();
+            sum += input;
+        } while (input != 0);
+
+//        while( (input = scanner.nextInt()) != 0) {
+//          sum += input;
+//        }
+
+        System.out.println(sum);
+    }
 }
--- test/src/Test.java
+++ test/src/Test.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 
-public class Main {
+/**
+ * @author UGURINAL
+ * on 9/4/2019
+ * at 6:15 PM
+ * test
+ */
 
+public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int count = input.nextInt();
-        int userNum = 0;
+        int sum = 0;
         int max = 0;
+        int userNum;
 
-        while (count != 0) {
+        while(sum <= 1000){
             userNum = input.nextInt();
-            if (userNum % 4 == 0 && userNum > max) {
+            input.nextLine();
+            sum += userNum;
+
+            if( userNum % 4 == 0 && userNum > max){
                 max = userNum;
             }
-            count--;
         }
         System.out.println(max);
+
     }
 }
