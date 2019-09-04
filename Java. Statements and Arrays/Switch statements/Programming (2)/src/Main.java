099fa5b9dc9011cd46b2b44babb8410b5b5bbdc6
Uğur İnal <ugurinal01@hotmail.com>
2019-09-04 16:12:05 +0300

    solution 2
    solution 2

--- Java. Statements and Arrays/Switch statements/Programming (2)/src/Main.java
+++ Java. Statements and Arrays/Switch statements/Programming (2)/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long n1 = input.nextLong();
+        char operator = input.next().charAt(0);
+        long n2 = input.nextLong();
+
+        switch (operator) {
+            case '+':
+                System.out.println(n1 + n2);
+                break;
+            case '-':
+                System.out.println(n1 - n2);
+                ;
+                break;
+            case '/':
+                if (n2 == 0) {
+                    System.out.println("Division by 0!");
+                } else {
+                    System.out.println(n1 / n2);
+                }
+                break;
+            case '*':
+                System.out.println(n1 * n2);
+                break;
+            default:
+                System.out.println("Unknown operator");
+        }
+    }
 }
