22e06cef20460380720922319d8a0b0574d4dfcd
Uğur İnal <ugurinal01@hotmail.com>
2019-09-05 17:02:50 +0300

    #Solution5
    #Solution5

--- Java. Statements and Arrays/While and do-while loops/Programming (5)/src/Main.java
+++ Java. Statements and Arrays/While and do-while loops/Programming (5)/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int times = scanner.nextInt();
+        int i = 0;
+        while (times > 0) {
+            i++;
+            int j = i;
+            while (j > 0 && times > 0) {
+                System.out.print(i + " ");
+                j--;
+                times--;
+            }
+        }
+    }
 }
