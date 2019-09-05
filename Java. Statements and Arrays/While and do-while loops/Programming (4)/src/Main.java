e8d84825e19d324bb8378fc79fd1c32e867b923a
Uğur İnal <ugurinal01@hotmail.com>
2019-09-05 17:02:44 +0300

    #Solution4
    #Solution4

--- Java. Statements and Arrays/While and do-while loops/Programming (4)/src/Main.java
+++ Java. Statements and Arrays/While and do-while loops/Programming (4)/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long num = scanner.nextLong();
+        int i = 1;
+        while (findFactorial(i) < num) {
+            i++;
+        }
+        System.out.println(i);
+    }
+
+    private static int findFactorial(int num) {
+        int result = 1;
+        for (int j = num; j > 0; j--) {
+            result = result * j;
+        }
+        return result;
+    }
 }
