c037bd308bdd45611a3491e593d0963ab5a30357
Uğur İnal <ugurinal01@hotmail.com>
2019-09-07 04:08:44 +0300

    #Solution3
    #Solution3

--- Java. Statements and Arrays/For loop/Programming (2)/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming (2)/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numOfElements = scanner.nextInt();
+        int element = 0;
+        int maxNumber = 0;
+
+        for (int i = 0; i < numOfElements; i++) {
+            element = scanner.nextInt();
+            if (element % 4 == 0 && element > maxNumber) {
+                maxNumber = element;
+            }
+        }
+        System.out.println(maxNumber);
+    }
 }
