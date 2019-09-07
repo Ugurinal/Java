7e0845358394d05f6a1bf22f4c90fa50bfb49554
Uğur İnal <ugurinal01@hotmail.com>
2019-09-07 04:08:09 +0300

    #Solution1
    #Solution1

--- Java. Statements and Arrays/For loop/Programming/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
 }
