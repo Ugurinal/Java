aed3d726fe8d2d5a3050417fa1b3eca453219b6a
Uğur İnal <ugurinal01@hotmail.com>
2019-09-07 04:07:26 +0300

    #8 AbstarctClasses
    #8 AbstarctClasses

---
+++ InnerAndAbstractClassesAndInterfaces/#8/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author UGURINAL
+ * on 9/5/2019
+ * at 5:10 PM
+ * AbstractClasses
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
