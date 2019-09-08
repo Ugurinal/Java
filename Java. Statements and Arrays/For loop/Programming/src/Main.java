ce134f992ea931a3e67625a770ad8ca547f88445
Uğur İnal <ugurinal01@hotmail.com>
2019-09-08 04:18:35 +0300

    #Solution0
    #Solution0

--- Java. Statements and Arrays/For loop/Programming/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int sum = 0;
