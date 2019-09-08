8185d098bb2120df54e030d35dd2afeb770b209d
Uğur İnal <ugurinal01@hotmail.com>
2019-09-08 04:19:15 +0300

    #Solution6
    #Solution6

--- Java. Statements and Arrays/For loop/Programming (6)/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming (6)/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = a; i <= b; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
 }
