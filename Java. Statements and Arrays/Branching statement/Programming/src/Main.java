8ae20f29a12956c4ff70bc41ee2e40bdc42e90ff
Uğur İnal <ugurinal01@hotmail.com>
2019-09-08 04:19:36 +0300

    #Solution0
    #Solution0

--- Java. Statements and Arrays/Branching statement/Programming/src/Main.java
+++ Java. Statements and Arrays/Branching statement/Programming/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+
+        while (true) {
+            number = scanner.nextInt();
+
+            if (number == 0) {
+                break;
+            } else if (number % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+
+    }
 }
