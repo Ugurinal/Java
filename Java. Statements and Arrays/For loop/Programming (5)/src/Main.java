17b100f506b4a7b72287775d021b8bb0da24c9a8
Uğur İnal <ugurinal01@hotmail.com>
2019-09-08 04:19:09 +0300

    #Solution5
    #Solution5

--- Java. Statements and Arrays/For loop/Programming (5)/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming (5)/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int aCounter = 0;
+        int bCounter = 0;
+        int cCounter = 0;
+        int dCounter = 0;
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int score = scanner.nextInt();
+
+            if (score == 2) {
+                aCounter++;
+            } else if (score == 3) {
+                bCounter++;
+            } else if (score == 4) {
+                cCounter++;
+            } else if (score == 5) {
+                dCounter++;
+            }
+        }
+        System.out.println(aCounter + " " + bCounter + " " + cCounter + " " + dCounter);
+    }
 }
