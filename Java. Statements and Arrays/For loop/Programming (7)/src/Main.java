c8a10caaacedaa3cab48cfa55714e7b66c964f54
Uğur İnal <ugurinal01@hotmail.com>
2019-09-08 04:19:19 +0300

    #Solution7
    #Solution7

--- Java. Statements and Arrays/For loop/Programming (7)/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming (7)/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int lCounter = 0;
+        int sCounter = 0;
+        int pCounter = 0;
+        int num = 0;
+
+        for (int i = 0; i < size; i++) {
+            num = scanner.nextInt();
+            if (num == 1) {
+                lCounter++;
+            } else if (num == -1) {
+                sCounter++;
+            } else if (num == 0) {
+                pCounter++;
+            }
+        }
+        System.out.println(pCounter + " " + lCounter + " " + sCounter);
+    }
 }
