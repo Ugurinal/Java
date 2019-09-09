2976757e8319b82d9c5f617e903865d26da4b505
Uğur İnal <ugurinal01@hotmail.com>
2019-09-09 23:33:57 +0300

    update
    update

--- Java. Statements and Arrays/Array/Programming/src/Main.java
+++ Java. Statements and Arrays/Array/Programming/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] myArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        boolean test = false;
+
+        for (int i = 0; i < size - 1; i++) {
+
+            if ((myArray[i] == n && myArray[i + 1] == m) || (myArray[i] == m && myArray[i + 1] == n)) {
+                test = true;
+                break;
+            }
+
+        }
+
+        System.out.println(test ? "true" : "false");
+    }
 }
