bc72126e7a2570bcd672da9831331977554bcf79
Uğur İnal <ugurinal01@hotmail.com>
2019-09-11 15:31:40 +0300

    update
    update

--- Java. Statements and Arrays/Array/Programming (3)/src/Main.java
+++ Java. Statements and Arrays/Array/Programming (3)/src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] myArray = new int[size];
+        int[] copyA;
+
+        for (int i = 0; i < size; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+
+        copyA = myArray.clone();
+
+        for (int i = 0; i < size; i++) {
+            if (i != size - 1) {
+                myArray[i + 1] = copyA[i];
+            } else {
+                myArray[0] = copyA[i];
+            }
+        }
+
+        for (int i : myArray) {
+            System.out.print(i + " ");
+        }
+
+    }
 }
