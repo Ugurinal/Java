b5c5caaa161b715cb44aa7fc1a108f2f32f909b6
Uğur İnal <ugurinal01@hotmail.com>
2019-09-11 04:49:54 +0300

    nothing
    nothing

--- Java. Statements and Arrays/For loop/Programming (6)/src/Main.java
+++ Java. Statements and Arrays/For loop/Programming (6)/src/Main.java
@@ -8,15 +8,18 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        for (int i = a; i <= b; i++) {
-            if (i % 15 == 0) {
+        for (; a <= b; a++) {
+            if (a % 15 == 0) {
+                System.out.println(a);
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (a % 3 == 0) {
+                System.out.println(a);
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (a % 5 == 0) {
+                System.out.println(a);
                 System.out.println("Buzz");
             } else {
-                System.out.println(i);
+                System.out.println(a);
             }
         }
 
