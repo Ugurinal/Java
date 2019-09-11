b5b2c9fcdfb3007e1d9285f66c865828c6748538
Uğur İnal <ugurinal01@hotmail.com>
2019-09-11 04:52:21 +0300

    #solution1
    #solution1

--- Java. Statements and Arrays/Array/Programming (1)/src/Main.java
+++ Java. Statements and Arrays/Array/Programming (1)/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int idx = 0;
+        int size = scanner.nextInt();
+        int[] myArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (myArray[idx] < myArray[i]) {
+                idx = i;
+            }
+        }
+        System.out.println(idx);
+    }
 }
