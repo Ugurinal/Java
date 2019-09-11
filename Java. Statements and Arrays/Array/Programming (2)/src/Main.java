ad3fa85db5d44e443b051848055dc440a6e8ba6f
Uğur İnal <ugurinal01@hotmail.com>
2019-09-11 04:52:27 +0300

    #solution2
    #solution2

--- Java. Statements and Arrays/Array/Programming (2)/src/Main.java
+++ Java. Statements and Arrays/Array/Programming (2)/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] myArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+
+        int sum = myArray[0];       // if it is 1 element
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j) continue;
+                if ((myArray[i] * myArray[j]) > sum) {
+                    sum = myArray[i] * myArray[j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
 }
