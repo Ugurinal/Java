a2d9ac2a2750adadcb8fc5c810e9487a8537cb85
Uğur İnal <ugurinal01@hotmail.com>
2019-09-12 19:45:12 +0300

    update
    update

--- Java. Statements and Arrays/Conditional statements/Programming/src/Main.java
+++ Java. Statements and Arrays/Conditional statements/Programming/src/Main.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int a = input.nextInt();
-    if(a > 0 ){
-      System.out.println("YES");
-    } else {
-      System.out.println("NO");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        if (a > 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        input.close();
     }
-    input.close();
-  }
 }
