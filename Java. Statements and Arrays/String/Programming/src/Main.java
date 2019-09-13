05b735f532ec6334996d68c084265c26a49b6d57
Uğur İnal <ugurinal01@hotmail.com>
2019-09-13 03:44:31 +0300

    update
    update

--- Java. Statements and Arrays/String/Programming/src/Main.java
+++ Java. Statements and Arrays/String/Programming/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String num = scanner.nextLine();
+        int first = 0;
+        int second = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            if (i < 3) {
+                first += num.charAt(i);
+            } else {
+                second += num.charAt(i);
+            }
+        }
+
+        System.out.println(first == second ? "Lucky" : "Regular");
+
+    }
 }
