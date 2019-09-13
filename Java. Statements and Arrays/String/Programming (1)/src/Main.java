71e49d845b78be9dcc9a8b4472244156d1130c02
Uğur İnal <ugurinal01@hotmail.com>
2019-09-13 03:59:36 +0300

    update
    update

--- Java. Statements and Arrays/String/Programming (1)/src/Main.java
+++ Java. Statements and Arrays/String/Programming (1)/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        String[] strings = text.split(" ");
+        int idx = 0;
+        int length = 0;
+
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() > length) {
+                length = strings[i].length();
+                idx = i;
+            }
+        }
+
+        System.out.println(strings[idx]);
+    }
 }
