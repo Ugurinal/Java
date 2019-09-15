b0b5e82ebf61e398b969a79c2db57b5c0b767d4c
UGURINAL <ugurinal01@hotmail.com>
2019-09-16 02:32:02 +0300

    #solution 4
    #solution 4

--- Java. Statements and Arrays/String/Programming (4)/src/Main.java
+++ Java. Statements and Arrays/String/Programming (4)/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        int counter = 0;
+        char temp = text.charAt(0);
+
+        for (int i = 0; i < text.length(); i++) {
+            if (temp == text.charAt(i)) {
+                counter++;
+            } else {
+                System.out.print(temp);
+                System.out.print(counter);
+                temp = text.charAt(i);
+                counter = 1;
+            }
+        }
+
+        System.out.print(temp);
+        System.out.print(counter);
+    }
 }
