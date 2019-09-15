ac4e83bcd947a4b98cf17891a4e8f9f1034825f2
UGURINAL <ugurinal01@hotmail.com>
2019-09-16 02:31:56 +0300

    #solution 3
    #solution 3

--- Java. Statements and Arrays/String/Programming (3)/src/Main.java
+++ Java. Statements and Arrays/String/Programming (3)/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        text = text.toLowerCase();
+        double counter = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 'g' || text.charAt(i) == 'c') {
+                counter++;
+            }
+        }
+
+        System.out.println((counter / text.length()) * 100);
+    }
 }
