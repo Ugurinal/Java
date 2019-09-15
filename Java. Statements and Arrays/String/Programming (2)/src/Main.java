068a5e065c741780fa92314d66eb863bfc39b00f
UGURINAL <ugurinal01@hotmail.com>
2019-09-16 01:12:36 +0300

    update
    update

--- Java. Statements and Arrays/String/Programming (2)/src/Main.java
+++ Java. Statements and Arrays/String/Programming (2)/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //put your code here
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < text.length() / 2; i++) {
+            if (text.charAt(i) != text.charAt(text.length() - (i + 1))) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? "yes" : "no");
+    }
 }
